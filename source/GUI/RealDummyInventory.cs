4c940d8b8a0c833a057315a027a3e27efda92fcf
Craluminum2413 <beware.14b@gmail.com>
2024-07-10 22:13:16 +0300

    return ahh... back ahh... dummy slots

---
+++ source/GUI/RealDummyInventory.cs
@@ -0,0 +1,33 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+
+namespace CombatOverhaul;
+
+public class RealDummyInventory : DummyInventory
+{
+    public RealDummyInventory(ICoreAPI api, int quantitySlots = 1) : base(api, quantitySlots)
+    {
+    }
+
+    public override bool CanContain(ItemSlot sinkSlot, ItemSlot sourceSlot)
+    {
+        return false;
+    }
+
+    public override bool CanPlayerAccess(IPlayer player, EntityPos position)
+    {
+        return false;
+    }
+
+    public override bool CanPlayerModify(IPlayer player, EntityPos position)
+    {
+        return false;
+    }
+
+    protected override ItemSlot NewSlot(int i)
+    {
+        ItemSlot slot = base.NewSlot(i);
+        slot.DrawUnavailable = false;
+        return slot;
+    }
+}
