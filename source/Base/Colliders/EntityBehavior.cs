6ea047e7e01aa55449027aa95ed7045ae6d3ca94
maltiez2 <maltiez2@gmail.com>
2024-10-06 15:06:20 +0300

    fix

--- source/Base/Colliders/EntityBehavior.cs
+++ source/Base/Colliders/EntityBehavior.cs
@@ -139,7 +139,7 @@ public sealed class CollidersEntityBehavior : EntityBehavior
     {
         if (api.World.Player.Entity.EntityId == entityPlayer.EntityId) return;
         if (!HasOBBCollider) return;
-        if ((entity.Api as ICoreClientAPI)?.Render.WireframeDebugRender.Entity! != true) return;
+        if ((entity.Api as ICoreClientAPI)?.Render.WireframeDebugRender.Entity != true) return;
 
         IShaderProgram? currentShader = api.Render.CurrentActiveShader;
         currentShader?.Stop();
