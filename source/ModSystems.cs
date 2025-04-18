80fabe921bd7ef4ddf2d22d6c54844adae6f6ff4
maltiez2 <maltiez2@gmail.com>
2025-04-18 14:39:15 +0300

    wip

--- source/ModSystems.cs
+++ source/ModSystems.cs
@@ -1,9 +1,12 @@
 ﻿using CombatOverhaul.Armor;
 using CombatOverhaul.Integration;
+using CombatOverhaul.Utils;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
 using Vintagestory.Client.NoObf;
+using Vintagestory.GameContent;
 using Vintagestory.Server;
 
 namespace CombatOverhaul;
@@ -23,5 +26,61 @@ public sealed class CombatOverhaulAdditionalSystem : ModSystem
     public override void AssetsFinalize(ICoreAPI api)
     {
         ArmorAutoPatcher.Patch(api);
+
+        if (api is ICoreClientAPI clientApi)
+        {
+            CheckStatusClientSide(clientApi);
+        }
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+
+        Disposed = true;
+    }
+
+    private bool Disposed = false;
+
+    private void CheckStatusClientSide(ICoreClientAPI api)
+    {
+        IInventory? gearInventory = GetGearInventory(api.World.Player.Entity);
+        if (gearInventory is not ArmorInventory)
+        {
+            string className = gearInventory == null ? "null" : LoggerUtil.GetCallerTypeName(gearInventory);
+            LoggerUtil.Error(api, this, $"Gear inventory class was replaced by some other mod, with {className}");
+            ThrowException(api, $"(Combat Overhaul) Gear inventory class was replaced with '{className}' by some other mod, shutting down the client. Report this issue into Combat Overhaul thread with client-main logs attached.");
+        }
+
+        bool immersiveFirstPersonMode = api.Settings.Bool["immersiveFpMode"];
+        if (immersiveFirstPersonMode)
+        {
+            LoggerUtil.Error(api, this, $"Immersive first person mode is enabled. It is not supported. Turn this setting off.");
+            AnnoyPlayer(api, "(Combat Overhaul) Immersive first person mode is enabled. It is not supported. Turn this setting off to prevent this message.", () => api.Settings.Bool["immersiveFpMode"]);
+        }
+    }
+    private static InventoryBase? GetGearInventory(Entity entity)
+    {
+        return entity.GetBehavior<EntityBehaviorPlayerInventory>().Inventory;
+    }
+    private static void ThrowException(ICoreAPI api, string message)
+    {
+        api.World.RegisterCallback(_ => throw new Exception(message), 1);
+    }
+    private void AnnoyPlayer(ICoreClientAPI api, string message, System.Func<bool> continueDelegate)
+    {
+        api.World.RegisterCallback(_ =>
+        {
+            api.TriggerIngameError(this, "error", message);
+            if (!Disposed && continueDelegate()) AnnoyPlayer(api, message, continueDelegate);
+        }, 5000);
+    }
+    private void PrintInChat(ICoreClientAPI api, string message)
+    {
+        api.World.RegisterCallback(_ =>
+        {
+            api.SendChatMessage(message);
+            if (!Disposed) PrintInChat(api, message);
+        }, 5000);
     }
 }
